3fa267c99465d71031bcd20bb6d675a191e85d31
Abe <abhi.rawat456@gmail.com>
2020-10-12 19:08:01 +0530

    Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
-import Auth from "./components/Auth";
-import Events from "./components/Events";
-import Bookings from "./components/Bookings";
+import Auth from "./components/auth/Auth";
+import Events from "./components/events/Events";
+import Bookings from "./components/bookings/Bookings";
+import MainNavigation from "./components/navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <MainNavigation />
       <Switch>
         <Redirect from="/" to="/auth" exact />
         <Route path="/auth" component={Auth} />
