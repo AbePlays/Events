7eaa7177f67a0aa9306aeb762d9f28c11e281a7a
Abe <abhi.rawat456@gmail.com>
2020-10-12 12:01:39 +0530

    Added login and jwt

--- Backend/graphql/resolvers/auth.js
+++ Backend/graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
 
 module.exports = {
@@ -21,6 +22,30 @@ module.exports = {
       }
     } catch (e) {
       console.log(e);
+      throw e;
+    }
+  },
+  login: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        throw new Error("User does not exist");
+      }
+      const isEqual = await bcrypt.compare(password, user.password);
+      if (!isEqual) {
+        throw new Error("Password is incorrect");
+      }
+      const token = jwt.sign(
+        {
+          userId: user.id,
+          email: user.email,
+        },
+        "somesecret",
+        { expiresIn: "1h" }
+      );
+
+      return { userId: user.id, token, tokenExpiration: 1 };
+    } catch (e) {
       throw e;
     }
   },
