28d11d31059223bc242227bf470b69947cd8b4bf
Abe <abhi.rawat456@gmail.com>
2020-10-12 11:05:30 +0530

    Added mutation to book and cancel event

--- Backend/graphql/resolvers/index.js
+++ Backend/graphql/resolvers/index.js
@@ -33,6 +33,19 @@ const user = async (userId) => {
   }
 };
 
+const singleEvent = async (eventId) => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator),
+    };
+  } catch (e) {
+    throw e;
+  }
+};
+
 module.exports = {
   events: async () => {
     try {
@@ -57,6 +70,8 @@ module.exports = {
         return {
           ...booking._doc,
           id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
           createdAt: new Date(booking._doc.createdAt).toISOString(),
           updatedAt: new Date(booking._doc.updatedAt).toISOString(),
         };
@@ -129,8 +144,24 @@ module.exports = {
     return {
       ...result._doc,
       id: result.id,
+      user: user.bind(this, booking._doc.user),
+      event: singleEvent.bind(this, booking._doc.event),
       createdAt: new Date(result._doc.createdAt).toISOString(),
       updatedAt: new Date(result._doc.updatedAt).toISOString(),
     };
+  },
+  cancelBooking: async (args) => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate("event");
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator),
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (e) {
+      throw e;
+    }
   },
 };
