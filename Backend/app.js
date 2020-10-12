365087fc491ba566160587bc5e0b062dcb66a013
Abe <abhi.rawat456@gmail.com>
2020-10-12 20:12:52 +0530

    Authorizing user into the application

--- Backend/app.js
+++ Backend/app.js
@@ -15,13 +15,22 @@ mongoose
     }
   )
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(1000, () => {
       console.log("Server available at port 3000");
     });
   })
   .catch((e) => console.log(e));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(isAuth);
 app.use(
   "/graphql",
--- frontend/src/components/auth/Auth.js
+++ frontend/src/components/auth/Auth.js
@@ -1,21 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    if (!isLogin && password !== confirmPassword) {
+      return;
+    }
+
+    let queryBody = {
+      query: `
+        query {
+          login(email : "${email}", password : "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `,
+    };
+
+    if (!isLogin) {
+      queryBody = {
+        query: `
+          mutation {
+            createUser(userInput : {
+              email : "${email}",
+              password : "${password}"
+            }) {
+              _id
+              email
+            }
+          }
+        `,
+      };
+    }
+
+    fetch("http://localhost:1000/graphql", {
+      method: "POST",
+      body: JSON.stringify(queryBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={submitHandler}>
       <div className="form-control">
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
+      {!isLogin && (
+        <div className="form-control">
+          <label htmlFor="password">Confirm Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+      )}
 
       <div className="form-actions">
-        <button type="submit">Login</button>
-        <button type="button">Signup</button>
+        {isLogin ? (
+          <div>
+            <button type="submit">Login</button>
+            <p>
+              Don't have an account?
+              <span onClick={() => setIsLogin((prev) => !prev)}> Sign up</span>
+            </p>
+          </div>
+        ) : (
+          <div>
+            <button type="submit">Sign up</button>
+            <p>
+              Already have an account?
+              <span onClick={() => setIsLogin((prev) => !prev)}> Login</span>
+            </p>
+          </div>
+        )}
       </div>
     </form>
   );
