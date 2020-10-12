5bc55c32cfb9d2a610d1e199088db679ae9cf82c
Abe <abhi.rawat456@gmail.com>
2020-10-12 19:27:06 +0530

    Created Login Page

--- frontend/src/components/auth/Auth.js
+++ frontend/src/components/auth/Auth.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./style.css";
 
 function Auth() {
-  return <h1>Auth Page</h1>;
+  return (
+    <form className="auth-form">
+      <div className="form-control">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+      </div>
+
+      <div className="form-actions">
+        <button type="submit">Login</button>
+        <button type="button">Signup</button>
+      </div>
+    </form>
+  );
 }
 
 export default Auth;
