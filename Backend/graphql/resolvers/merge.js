2fa7f1fbf971ba544749e9aa5c50445683ac4451
Abe <abhi.rawat456@gmail.com>
2020-10-13 18:44:42 +0530

    Using dataloader to optimize api calls

--- Backend/graphql/resolvers/merge.js
+++ Backend/graphql/resolvers/merge.js
@@ -1,6 +1,15 @@
 const Event = require("../../models/event");
 const User = require("../../models/user");
+const DataLoader = require("dataloader");
 const { dateToString } = require("../../helpers/date");
+
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const transformEvent = (event) => {
   return {
@@ -35,11 +44,11 @@ const events = async (eventIds) => {
 
 const user = async (userId) => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents),
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents),
     };
   } catch (e) {
     throw e;
@@ -48,8 +57,8 @@ const user = async (userId) => {
 
 const singleEvent = async (eventId) => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (e) {
     throw e;
   }
