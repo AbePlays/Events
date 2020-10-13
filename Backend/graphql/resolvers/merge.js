665db2ff52957b3901b57c7cdc21ba862c61f84c
Abe <abhi.rawat456@gmail.com>
2020-10-13 18:49:06 +0530

    Bug Fix

--- Backend/graphql/resolvers/merge.js
+++ Backend/graphql/resolvers/merge.js
@@ -48,7 +48,7 @@ const user = async (userId) => {
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents),
+      createdEvents: () => eventLoader.loadMany(user._doc.createdEvents),
     };
   } catch (e) {
     throw e;
