583e5fb58765ba7b14f2dcc9a44f76273bb803f5
Abe <abhi.rawat456@gmail.com>
2020-10-13 17:41:22 +0530

    Showing Bookings made by the user

--- frontend/src/components/bookings/Bookings.js
+++ frontend/src/components/bookings/Bookings.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import "./style.css";
 
 function Bookings() {
-  return <h1>Booking Page</h1>;
+  const [bookings, setBookings] = useState([]);
+  const { token } = useContext(AuthContext);
+
+  const fetchBookings = () => {
+    const queryBody = {
+      query: `
+        query {
+          bookings {
+            _id
+            createdAt
+            event {
+              _id
+              title
+              date
+            }
+          }
+        }
+      `,
+    };
+
+    fetch("http://localhost:1000/graphql", {
+      method: "POST",
+      body: JSON.stringify(queryBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        const data = res.data.bookings;
+        setBookings(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    fetchBookings();
+  }, []);
+
+  return (
+    <ul>
+      {bookings.map((booking) => (
+        <li>{booking.event.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default Bookings;
--- frontend/src/components/events/Event.js
+++ frontend/src/components/events/Event.js
@@ -1,13 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Backdrop from "../modal/Backdrop";
 import Modal from "../modal/Modal";
+import { AuthContext } from "../../context/AuthContext";
 
 function Event(props) {
   const [isDark, setIsDark] = useState(false);
+  const { token } = useContext(AuthContext);
+
   var date = new Date(props.date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
   });
+
+  const bookEvent = () => {
+    const queryBody = {
+      query: `
+          mutation {
+            bookEvent(eventId: "${props.eventId}") {
+              _id
+              createdAt
+              updatedAt
+            }
+          }
+        `,
+    };
+
+    fetch("http://localhost:1000/graphql", {
+      method: "POST",
+      body: JSON.stringify(queryBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        console.log(res);
+        setIsDark(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   date = date.split(" ");
   const month = date[0];
   const day = date[1];
@@ -21,6 +61,7 @@ function Event(props) {
           canConfirm
           setIsDark={setIsDark}
           confirmTitle="Book"
+          confirmHandler={bookEvent}
         >
           <h3>{`$${props.price} - ${month}, ${day}`}</h3>
           <br />
